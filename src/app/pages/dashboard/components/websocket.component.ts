18d73602c3a2dc40c176f99c72f903eadf73393a
jefaokpta <jefaokpta@hotmail.com>
2025-10-23 15:11:56 -0300

    recebendo msgs [websocket] Ajusta endpoints WebSocket e adiciona log de processamento
    - Alterado o destino do WebSocket de "/topic/channel-messages" para "/topic/active-channels" para refletir a nova estrutura de canais ativos.
    - Adicionado um log no KafkaListenerService para inspecionar mensagens processadas, facilitando o debug e monitoramento.

--- src/app/pages/dashboard/components/websocket.component.ts
+++ src/app/pages/dashboard/components/websocket.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit} from "@angular/core";
+import {Component, OnDestroy} from "@angular/core";
 import {WebsocketService} from "@/websocket/stomp/websocket.service";
 import {Subscription} from "rxjs";
 import {rxStompServiceFactory} from "@/websocket/stomp/rx-stomp-service-factory";
@@ -8,18 +8,15 @@ import {rxStompServiceFactory} from "@/websocket/stomp/rx-stomp-service-factory"
     providers: [{provide: WebsocketService, useFactory: rxStompServiceFactory}],
     template: '<p>websocket works!</p>'
 })
-export class WebsocketComponent implements OnInit, OnDestroy {
+export class WebsocketComponent implements OnDestroy {
 
     private readonly webSocketSubscription: Subscription;
 
     constructor(private readonly webSocketService: WebsocketService) {
-        this.webSocketSubscription = this.webSocketService.watch("/topic").subscribe(message => {
+        this.webSocketSubscription = this.webSocketService.watch("/topic/active-channels").subscribe(message => {
             console.log(message);
+            console.log(message.body);
         })
-    }
-
-    ngOnInit(): void {
-
     }
 
     ngOnDestroy(): void {
