15e1aa566a3c73af311095f7e5bf158a00a89662
jefaokpta <jefaokpta@hotmail.com>
2026-02-05 18:03:47 -0300

    nao executa submit [master] Substituir InputText por Password no formulário de ativação de ramal
    - Trocado o componente `pInputText` por `pPassword` no campo `peerSecret`, permitindo a utilização de máscara de senha com alternância de visibilidade (`toggleMask`).
    - Removida importação não utilizada de `InputText` e adicionada importação de `Password` ao componente.
    - Ajustada a submissão do formulário para remover erro de sintaxe Angular no atributo `(ngSubmit)`.
    
    Motivo: Melhorar a segurança e a experiência do usuário ao digitar a senha no formulário.

--- src/app/layout/components/activate-peer-dialog.component.ts
+++ src/app/layout/components/activate-peer-dialog.component.ts
@@ -1,25 +1,25 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Button } from 'primeng/button';
 import { Dialog } from 'primeng/dialog';
-import { InputText } from 'primeng/inputtext';
 import { NgIf } from '@angular/common';
 import { PeerSelectComponent } from '@/pabx/dialplan/components/peer-select-component';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Password } from 'primeng/password';
 
 @Component({
     selector: 'app-activate-peer-dialog-component',
     standalone: true,
-    imports: [Button, Dialog, InputText, NgIf, PeerSelectComponent, ReactiveFormsModule],
+    imports: [Button, Dialog, NgIf, PeerSelectComponent, ReactiveFormsModule, Password],
     template: `
         <ng-container *ngIf="isPeerFormDialogVisible">
             <p-dialog header="Ative seu ramal" [visible]="true" [modal]="true" [closable]="false">
-                <form [formGroup]="form" (ngSubmit)="submit()" class="p-fluid">
+                <form [formGroup]="form" (submit)="submit()" class="p-fluid">
                     <div class="field mb-4">
                         <app-peer-select-component formControlName="peer" [isOnlyWSS]="true" [showError]="true" />
                     </div>
                     <div class="field mb-4">
                         <label for="peerSecret" class="block mb-2">Senha *</label>
-                        <input id="peerSecret" pInputText class="p-inputtext" formControlName="peerSecret" />
+                        <p-password formControlName="peerSecret" [toggleMask]="true" feedback="false" />
                         <small
                             *ngIf="peerSecret?.invalid && (peerSecret?.dirty || peerSecret?.touched)"
                             class="p-error block mt-2"
@@ -62,9 +62,6 @@ export class ActivatePeerDialogComponent implements OnInit {
         console.log(this.form.value);
     }
 
-    get peer() {
-        return this.form.get('peer');
-    }
     get peerSecret() {
         return this.form.get('peerSecret');
     }
